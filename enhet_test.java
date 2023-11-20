7813cbc96f8fcc98efe7d00c957d9ed54871e5ff
Mohamed Ali <kahano243@gmail.com>
2023-11-20 16:11:55 +0100

    added some comments to Lioncompetiton file added

---
+++ enhet_test.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+class enhet_test{
+    public static void main(String[] args) {
+       
+        // String descp1 = "marry";
+        // int height1 = 300;
+        // String descp2 = "rob";
+        // int height2 = 250;
+
+        // LionDescription Lion_descp1 = new LionDescription(descp1,height1);
+        // LionDescription Lion_descp2 = new LionDescription(descp2,height2);
+        
+        // List<LionDescription> list_descp = List.of(Lion_descp1,Lion_descp2);
+        
+        
+        // String marry = "marry";
+        // int entertime_marry = 10;
+        // int exittime_marry = 15;
+        // LionSchedule schd1 = new LionSchedule(marry,entertime_marry,exittime_marry);
+
+        
+        // String rob = "rob";
+        // int entertime_rob = 13;
+        // int exittime_rob = 20;
+
+        //  LionSchedule schd2 = new LionSchedule(rob,entertime_rob,exittime_rob);
+
+        // List<LionSchedule> list_schd = List.of(schd1,schd2);
+        // System.out.println(list_schd);
+
+       
+        // LionCompetition competition = new LionCompetition(list_descp, list_schd);
+        // competition.LionEntered(10, 300); 
+        // competition.LionEntered(13, 250); 
+        // competition.LionLeft(20,250);
+        return;
+    }
+}
