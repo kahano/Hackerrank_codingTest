a52fe581aa3fbee4548ed9af6c8fd57a7f12fa32
Mohamed Ali <kahano243@gmail.com>
2023-11-20 17:44:06 +0100

    added some comments to Lioncompetiton file added

--- LionDescription.java
+++ LionDescription.java
@@ -3,26 +3,6 @@ class LionDescription {
         public String name;
         public int height;
 
-        // public String getName() {
-        //         return name;
-        // }
-        //
-        // public int getHeight() {
-        //         return height;
-        // }
-        //
-        // public LionDescription(String name, int height) {
-        //         this.name = name;
-        //         this.height = height;
-        // }
-        //
-        //
-        // @Override
-        // public String toString() {
-        //         return "{" +
-        //                 " name='" + getName() + "'" +
-        //                 ", height='" + getHeight() + "'" +
-        //                 "}";
-        // }
+        
 
 }
--- LionSchedule.java
+++ LionSchedule.java
@@ -2,30 +2,5 @@ class LionSchedule {
         public String name;
         public int enterTime;
         public int exitTime;
-        //
-        // public LionSchedule(String name, int enterTime, int exitTime) {
-        //         this.name = name;
-        //         this.enterTime = enterTime;
-        //         this.exitTime = exitTime;
-        // }
-        // public String getName() {
-        //         return name;
-        // }
-        // public int getEnterTime() {
-        //         return enterTime;
-        // }
-        // public int getExitTime() {
-        //         return exitTime;
-        // }
-        //
-        //
-        // @Override
-        // public String toString() {
-        //         return "{" +
-        //                 " name='" + getName() + "'" +
-        //                 ", enterTime='" + getEnterTime() + "'" +
-        //                 ", exitTime='" + getExitTime() + "'" +
-        //                 "}";
-        // }
-
+        
 }
